ec3fb0db4cb4b72da1504ddc7b2aa5ab2f6a2f1a
iAmNoice <94944632+iAmNoice@users.noreply.github.com>
2021-12-15 16:32:15 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,62 +1,53 @@
 #include "header.hpp"
+#include "expression.hpp"
 #include "parser.hpp"
+#include "equation.hpp"
 
 using namespace std;
 
-/*string remove_zeros(string str)
+void print(const vector<Token>& v)
 {
-    str.erase ( str.find_last_not_of('0') + 1, string::npos );
-    if(str[str.size()-1] == '.') {
-        str.erase ( str.find_last_not_of('.') + 1, string::npos );
+    for(int i = 0; i<v.size(); ++i) {
+        cout << v[i].type << " | " << v[i].num << " | " << v[i].var << endl;
     }
-    return str;
-}*/
+}
 
-double primary(Token&);
-double term(Token&);
-double expression(Token&);
-double equation(Token&, const char&);
-
-double equation(Token& t, const char& var) // solves for variable
+void print_expression(const vector<Token>& v)
 {
-    //double constant = expression(t, '');
-    double left = expression(t);
-    double right;
+    for(int i = 0; i<v.size(); ++i) {
+        cout << v[i].num << v[i].var << v[i].type;
+    }
+}
+
+/*void input(Expression& e)
+{
+    Token t;
+    char ch;
     while(true) {
-        right -= left;
+        cin.get(ch);
+        if(ch == '\n' || ch == '=') { break; }
+        cin.unget();
+        e.exp.push_back(t.get());
     }
-}
-
-double expression(Token& t) // combines like terms
-{
-    double c = term(t);
-
-}
-
-double term(Token& t) // distributes if needed
-{
-    double c = primary(t);
-}
-
-double primary(Token& t) // gets numbers
-{
-    //t.get();
-    //if(t.type == 'n' && t.var == )
-}
+    e.is_valid(e.exp);
+    /*while(true) {
+        cin.get(ch);
+        if(ch == '\n') { break; }
+        cin.unget();
+        eq.right.push_back(t.get());
+    }*/
+//}
 
 int main()
 {
-    Token t;
-    Token_stream ts;
-    string str;
-    cin >> str;
-    ts.input << str;
-    char ch;
-    while(ts.input.get(ts.temp)) {
-        if(ch == '\n') { break; }
-        ts.input.unget();
-        t = Token();
-        t.get(ts);
-        cout << t.type << " | " << t.num << " | " << t.var << endl;
-    }
+    Equation eq;
+    Expression e;
+
+    input(e);
+    print(e.exp);
+    cout << "============" << endl;
+    //print(eq.right);
+
+    e.simplify(e.exp);
+    print(e.exp);
 }
